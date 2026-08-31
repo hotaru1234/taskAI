c9cbcc64f977d92fd63d399035d4419f52d04027
hotaru1234 <vinhphucthan319@gmail.com>
2026-08-31 09:49:18 +0700

    hotaru
    space

---
+++ task11.py
@@ -0,0 +1,4 @@
+length, width = map(float, input("length , width").split(","))
+perimeter = (length + width) * 2
+area = (length * width)
+print("perimeter,area: ",perimeter,area)
---
+++ task4.py
@@ -0,0 +1,7 @@
+score = 10
+if score >= 9.5:
+    print("number of excellent students")
+elif score >=7:
+    print("number of student with good academic performance") 
+else:
+    print("average number of students ")  
